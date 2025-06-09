17452114375ed557a105481580cc22c7889359d4
Darwin Torrez <torrescarballo7@gmail.com>
2025-06-08 21:24:09 -0600

    Fixes

--- Taller/Models/Cita.cs
+++ Taller/Models/Cita.cs
@@ -7,18 +7,11 @@ namespace Taller.Models;
 public partial class Cita
 {
     public int IdCita { get; set; }
-
     public int IdCliente { get; set; }
-
     public DateOnly FechaCita { get; set; }
-
     public string? Estado { get; set; }
-
     public string? Descripcion { get; set; }
-
     public TimeOnly? Hora { get; set; }
     public bool Notificada { get; set; } // Nueva propiedad
-
-
     public virtual Cliente? IdClienteNavigation { get; set; } = null!;
 }
--- TallerWEBAPI/Models/Cita.cs
+++ TallerWEBAPI/Models/Cita.cs
@@ -6,15 +6,10 @@ namespace TallerWEBAPI.Models;
 public partial class Cita
 {
     public int IdCita { get; set; }
-
     public int IdCliente { get; set; }
-
     public DateOnly FechaCita { get; set; }
-
     public string? Estado { get; set; }
-
     public string? Descripcion { get; set; }
-
     public TimeOnly? Hora { get; set; }
     public virtual Cliente? IdClienteNavigation { get; set; } = null!;
 }
