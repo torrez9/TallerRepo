bdd195acc751ccbdd16c6fbdb60932ba86b5f59d
Darwin Torrez <torrescarballo7@gmail.com>
2025-06-08 19:11:42 -0600

    HTTP request

--- TallerWEBAPI/Program.cs
+++ TallerWEBAPI/Program.cs
@@ -109,14 +109,11 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TallerWEBAPI v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TallerWEBAPI v1");
+});
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");  
